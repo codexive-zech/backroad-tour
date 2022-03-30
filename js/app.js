fd112c0f93f638ce1fc952170acd14848620e678
Codexive_zech <zechariahhounwanou@gmail.com>
2022-03-30 14:13:55 +0100

    completed Backroad Flexbox P

--- js/app.js
+++ js/app.js
@@ -1,11 +1,8 @@
 // ********** set date ************
-// select span
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
 
 // ********** nav toggle ************
-// select button and links
 const navBtn = document.getElementById("nav-toggle");
 const links = document.getElementById("nav-links");
 // add event listener
@@ -14,10 +11,9 @@ navBtn.addEventListener("click", () => {
 });
 
 // ********** smooth scroll ************
-// select links
 const scrollLinks = document.querySelectorAll(".scroll-link");
-scrollLinks.forEach(link => {
-  link.addEventListener("click", e => {
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
     // prevent default
     e.preventDefault();
     links.classList.remove("show-links");
@@ -31,7 +27,7 @@ scrollLinks.forEach(link => {
       left: 0,
       // top: element.offsetTop,
       top: position,
-      behavior: "smooth"
+      behavior: "smooth",
     });
   });
 });
